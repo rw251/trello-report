90320c573ce2f8614821794115a630b1bff931fb
Richard Williams <richard.williams2@manchester.ac.uk>
2016-09-27 11:41:28 +0100

    To work on heroku

--- brunch-config.js
+++ brunch-config.js
@@ -1,13 +1,24 @@
 module.exports = {
   // See http://brunch.io for documentation.
+  paths: {
+    public: 'dist',
+    watched: ['app','vendor','server']
+  },
+
   files: {
     javascripts: {
       joinTo: {
-        'libraries.js': /^(?!app\/)/,
-        'app.js': /^app\//
+        'libraries.js': /^(?!(app|server)\/)/
       },
       order: {
-        before: [ /jquery/ ]
+        before: [
+          'vendor/scripts/bootstrap.js'
+          ]
+      },
+      entryPoints: {
+        'app/application.js': {
+          'app.js': /^app\//
+        }
       }
     },
     stylesheets: { joinTo: 'app.css' },
